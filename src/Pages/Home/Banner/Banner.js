bc045026e3ffe7b24cee3be56ed4ffdbe1e2c4da
shafik720 <shafikrasel5@gmail.com>
2022-12-04 20:58:12 +0600

    working on banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -6,22 +6,25 @@ const Banner = () => {
     return (
         <div className='banner-div container'>
             <div className="row g-3">
-            <div className="col-lg-6">
-                <div className="h-100">
-                    <div className="banner-left h-100">
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem consequuntur veniam aliquid. Minima facilis veniam consequatur sed itaque? Ipsa, sapiente facilis sunt voluptatibus exercitationem quo nemo, repellendus sit architecto culpa expedita soluta mollitia amet temporibus dolor totam numquam autem maxime quae dolorum doloremque aspernatur. Animi deserunt delectus vero ea nihil blanditiis vel, laborum quo necessitatibus? Ullam alias deleniti minima rerum eaque non numquam debitis. Modi, porro aliquid! Veniam, iure ut?</p>
-                    </div>
-                </div>
-            </div>
-            <div className="col-lg-6">
-                <div className="h-100">
-                    <div className="banner-right">
-                        <div className="banner-img">
-                            <img src={bannerImg} alt="" />
+                <div className="col-lg-6">
+                    <div className="h-100">
+                        <div className="banner-left h-100">
+                            <div className="banner-left-header">
+                                <p>Hi There</p>
+                                <h2>I am Jonathan Wick</h2>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                <div className="col-lg-6">
+                    <div className="h-100">
+                        <div className="banner-right">
+                            <div className="banner-img">
+                                <img src={bannerImg} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
