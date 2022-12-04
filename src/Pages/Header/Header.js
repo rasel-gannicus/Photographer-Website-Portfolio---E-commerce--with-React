4e27a5c919b68e5bc4f81dbacdd4b9988266e2be
shafik720 <shafikrasel5@gmail.com>
2022-12-04 17:54:40 +0600

    working on header

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Header File</h2>
+        <div className='header-div'>
+            <NavLink    to="/" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Home</NavLink>
+                <NavLink    to="/reviews" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Reviews</NavLink>
+                <NavLink   to="/dashboard" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Dashboard</NavLink>
+                <NavLink   to="/blogs" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Blogs</NavLink>
+                <NavLink   to="/about" className={({ isActive }) => (isActive ? 'active' : 'inactive')} >About</NavLink>
         </div>
     );
 };
