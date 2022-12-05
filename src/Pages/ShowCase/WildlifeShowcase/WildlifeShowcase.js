8ce90107d594114094e3a24f96d8412f0e80c65c
shafik720 <shafikrasel5@gmail.com>
2022-12-05 18:10:13 +0600

    working on potrait showcase

--- src/Pages/ShowCase/WildlifeShowcase/WildlifeShowcase.js
+++ src/Pages/ShowCase/WildlifeShowcase/WildlifeShowcase.js
@@ -6,22 +6,22 @@ const WildlifeShowcase = () => {
         <div className='row g-4 protrait-showCase  mx-auto h-100'>
             <div className="col-6">
                 <div className="">
-                    <img src="https://i.ibb.co/wN86NH1/craig-manners-bm-Kb-Wks7pps-unsplash-1.jpg" alt="" />
+                    <img src="https://i.ibb.co/6gJkch7/alexander-andrews-m-Ed-Ku-PYJe1-I-unsplash-1.jpg" alt="" />
                 </div>
             </div>
             <div className="col-6">
                 <div className="">
-                    <img src="https://i.ibb.co/wN86NH1/craig-manners-bm-Kb-Wks7pps-unsplash-1.jpg" alt="" />
+                    <img src="https://i.ibb.co/Z8PV2W9/matthew-spiteri-Wf-Z4-WCu-Ntlg-unsplash-1.jpg" alt="" />
                 </div>
             </div>
             <div className="col-6">
                 <div className="">
-                    <img src="https://i.ibb.co/wN86NH1/craig-manners-bm-Kb-Wks7pps-unsplash-1.jpg" alt="" />
+                    <img src="https://i.ibb.co/cwqVQ9S/tim-umphreys-Uop-R2-NUBYek-unsplash-1.jpg" alt="" />
                 </div>
             </div>
             <div className="col-6">
                 <div className="">
-                    <img src="https://i.ibb.co/wN86NH1/craig-manners-bm-Kb-Wks7pps-unsplash-1.jpg" alt="" />
+                    <img src="https://i.ibb.co/ZVCfDMM/geran-de-klerk-N9s3-Fjzsst-M-unsplash-1.jpg" alt="" />
                 </div>
             </div>
         </div>
