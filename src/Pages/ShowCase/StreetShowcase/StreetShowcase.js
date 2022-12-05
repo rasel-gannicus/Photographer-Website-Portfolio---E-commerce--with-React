a0ca2c32c36a8a333ba50bf7e83f9b0702e857f5
shafik720 <shafikrasel5@gmail.com>
2022-12-05 14:21:22 +0600

    working on potrait showcase

--- src/Pages/ShowCase/StreetShowcase/StreetShowcase.js
+++ src/Pages/ShowCase/StreetShowcase/StreetShowcase.js
@@ -6,12 +6,12 @@ const StreetShowcase = () => {
         <div className='row g-4 protrait-showCase  mx-auto h-100'>
             <div className="col-6">
                 <div className="">
-                    <img src="https://i.ibb.co/VCQGmkq/joey-h-Pwdajn-K3-M4-unsplash.jpg" alt="" />
+                    <img src="https://i.ibb.co/3Rzb4Yk/lahiru-edirisooriya-Re34b-HUab-IA-unsplash-1.jpg" alt="" />
                 </div>
             </div>
             <div className="col-6">
                 <div className="">
-                    <img src="https://i.ibb.co/VCQGmkq/joey-h-Pwdajn-K3-M4-unsplash.jpg" alt="" />
+                    <img src="https://i.ibb.co/NSDBZ18/natalia-trofimova-QFNso-Xm-CUA4-unsplash-1.jpg" alt="" />
                 </div>
             </div>
             <div className="col-6">
