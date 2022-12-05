1d23755883b652008f7434dd70afeda69a5537a5
shafik720 <shafikrasel5@gmail.com>
2022-12-05 18:43:42 +0600

    working on making responsive menu with vanilla js, html, css

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 
 const Header = () => {
     return (
         <div className='header-div'>
             <div className="header-main-parent">
-            <div className="header-main">
-                <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Home</NavLink>
-                <NavLink to="/reviews" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Reviews</NavLink>
-                <NavLink to="/dashboard" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Dashboard</NavLink>
-                <NavLink to="/blogs" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Blogs</NavLink>
-                <NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : 'inactive')} >About</NavLink>
+                <div className="header-main">
+                    <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Home</NavLink>
+                    <NavLink to="/reviews" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Reviews</NavLink>
+                    <NavLink to="/dashboard" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Dashboard</NavLink>
+                    <NavLink to="/blogs" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Blogs</NavLink>
+                    <NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : 'inactive')} >About</NavLink>
+                </div>
             </div>
+            <div className="header-icon">
+                <h2>PhotoGrapher</h2>
+                <span> <FontAwesomeIcon icon={faBars}></FontAwesomeIcon> </span>
             </div>
         </div>
     );
