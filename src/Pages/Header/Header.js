fed492c8109cb80bc2245378cfb7d7db51e874d1
shafik720 <shafikrasel5@gmail.com>
2022-12-05 18:33:47 +0600

    working on making responsive menu with vanilla js, html, css

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -5,12 +5,14 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='header-div'>
+            <div className="header-main-parent">
             <div className="header-main">
                 <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Home</NavLink>
                 <NavLink to="/reviews" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Reviews</NavLink>
                 <NavLink to="/dashboard" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Dashboard</NavLink>
                 <NavLink to="/blogs" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Blogs</NavLink>
                 <NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : 'inactive')} >About</NavLink>
+            </div>
             </div>
         </div>
     );
