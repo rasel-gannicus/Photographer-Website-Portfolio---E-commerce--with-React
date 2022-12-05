413260be6f9d60781110fc0046a4973d77ac97c6
shafik720 <shafikrasel5@gmail.com>
2022-12-05 14:06:31 +0600

    working on potrait showcase

--- src/Pages/ShowCase/PortraitShowcase/PortraitShowcase.js
+++ src/Pages/ShowCase/PortraitShowcase/PortraitShowcase.js
@@ -4,27 +4,27 @@ import './PortraitShowcase.css';
 const PortraitShowcase = () => {
     return (
         <div className='row g-4 protrait-showCase  mx-auto h-100'>
-                <div className="col-6">
-                    <div className="">
-                        <img src="https://i.ibb.co/wN86NH1/craig-manners-bm-Kb-Wks7pps-unsplash-1.jpg" alt="" />
-                    </div>
-                </div>
-                <div className="col-6">
-                    <div className="">
-                        <img src="https://i.ibb.co/wN86NH1/craig-manners-bm-Kb-Wks7pps-unsplash-1.jpg" alt="" />
-                    </div>
-                </div>
-                <div className="col-6">
-                    <div className="">
-                        <img src="https://i.ibb.co/wN86NH1/craig-manners-bm-Kb-Wks7pps-unsplash-1.jpg" alt="" />
-                    </div>
-                </div>
-                <div className="col-6">
-                    <div className="">
-                        <img src="https://i.ibb.co/wN86NH1/craig-manners-bm-Kb-Wks7pps-unsplash-1.jpg" alt="" />
-                    </div>
+            <div className="col-6">
+                <div className="">
+                    <img src="https://i.ibb.co/gRcPr6d/tyler-nix-ZGa9d1a-4t-A-unsplash-1.jpg" alt="" />
                 </div>
             </div>
+            <div className="col-6">
+                <div className="">
+                    <img src="https://i.ibb.co/JRw7CcC/christopher-campbell-Cp-LUHPRp-WM-unsplash.jpg" alt="" />
+                </div>
+            </div>
+            <div className="col-6">
+                <div className="">
+                    <img src="https://i.ibb.co/qsFHB2f/umar-kashif-u0g-H1-D-GJw-E-unsplash-2.jpg" alt="" />
+                </div>
+            </div>
+            <div className="col-6">
+                <div className="">
+                    <img src="https://i.ibb.co/843HCxZ/albert-dera-ILip77-Sbm-OE-unsplash-1.jpg" alt="" />
+                </div>
+            </div>
+        </div>
     );
 };
 
