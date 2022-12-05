f93550a21dee07003cb91db3bb9c6c9d729f4d7a
shafik720 <shafikrasel5@gmail.com>
2022-12-05 11:09:35 +0600

    about me section done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,8 +3,12 @@ import './About.css';
 
 const About = () => {
     return (
-        <div className='about-div'>
-            <h2>Know about us</h2>
+        <div className='container about-div'>
+            <div className="container">
+                <h2>Who am i ?</h2>
+                <p>A Professional Photographer from a Small City in Bangladesh. I am a self taught Nerd. I have been doing Photography for more than a Decade. I have won several awards and four International Awards for Photography. </p>
+                <p>Street & Wild-life Photography is my favourite Genre. I also have a good hand In Wedding & Portfolio Area. Some of my sample works are given below.  </p>
+            </div>
         </div>
     );
 };
