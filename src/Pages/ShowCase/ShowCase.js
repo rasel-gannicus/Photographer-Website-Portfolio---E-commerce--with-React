22b3960ba9dd0633753b78ed72e5034916accb0b
shafik720 <shafikrasel5@gmail.com>
2022-12-05 12:14:55 +0600

    working on showcase

--- src/Pages/ShowCase/ShowCase.js
+++ src/Pages/ShowCase/ShowCase.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import PortraitShowcase from './PortraitShowcase/PortraitShowcase';
 import './ShowCase.css';
+import StreetShowcase from './StreetShowcase/StreetShowcase';
+import WeddingShowcase from './WeddingShowcase/WeddingShowcase';
+import WildlifeShowcase from './WildlifeShowcase/WildlifeShowcase';
 
 const ShowCase = () => {
     return (
-        <div className='container showCase-div'>
+        <div className='container showCase-div border mx-auto'>
             <h2>ShowCase here</h2>
             <div className="row">
-                <div className="col-lg-6">
-                    <div className=""></div>
+                <div className="col-lg-6 ">
+                    <div className="">
+                        <PortraitShowcase></PortraitShowcase>
+                    </div>
                 </div>
                 <div className="col-lg-6">
-                    <div className=""></div>
+                    <div className="">
+                        <StreetShowcase></StreetShowcase>
+                    </div>
                 </div>
                 <div className="col-lg-6">
-                    <div className=""></div>
+                    <div className="">
+                        <WeddingShowcase></WeddingShowcase>
+                    </div>
                 </div>
                 <div className="col-lg-6">
-                    <div className=""></div>
+                    <div className="">
+                        <WildlifeShowcase></WildlifeShowcase>
+                    </div>
                 </div>
             </div>
         </div>
