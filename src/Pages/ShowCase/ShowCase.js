99ec8734548c12089557c23328c7508619416f0b
shafik720 <shafikrasel5@gmail.com>
2022-12-05 12:01:37 +0600

    working on showcase

--- src/Pages/ShowCase/ShowCase.js
+++ src/Pages/ShowCase/ShowCase.js
@@ -5,6 +5,20 @@ const ShowCase = () => {
     return (
         <div className='container showCase-div'>
             <h2>ShowCase here</h2>
+            <div className="row">
+                <div className="col-lg-6">
+                    <div className=""></div>
+                </div>
+                <div className="col-lg-6">
+                    <div className=""></div>
+                </div>
+                <div className="col-lg-6">
+                    <div className=""></div>
+                </div>
+                <div className="col-lg-6">
+                    <div className=""></div>
+                </div>
+            </div>
         </div>
     );
 };
