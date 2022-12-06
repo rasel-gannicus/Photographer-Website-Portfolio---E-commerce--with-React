9d661742b0e634bfb38414f5bfb1c95db96d5a53
shafik720 <shafikrasel5@gmail.com>
2022-12-06 19:21:12 +0600

    working on popup display

---
+++ src/Pages/ShowCase/StreetDisplay/StreetDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './StreetDisplay.css';
+
+const StreetDisplay = () => {
+    return (
+        <div>
+            <h2>Street Photography</h2>
+        </div>
+    );
+};
+
+export default StreetDisplay;
---
+++ src/Pages/ShowCase/WeddingDisplay/WeddingDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WeddingDisplay.css';
+
+const WeddingDisplay = () => {
+    return (
+        <div>
+            <h2>Wedding Display Show</h2>
+        </div>
+    );
+};
+
+export default WeddingDisplay;
