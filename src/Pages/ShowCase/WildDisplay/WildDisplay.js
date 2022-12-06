796e64686edc7b37fb89855b6c19ee95116e07c2
shafik720 <shafikrasel5@gmail.com>
2022-12-06 19:22:47 +0600

    working on popup display

---
+++ src/Pages/ShowCase/WildDisplay/WildDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WildDisplay.css';
+
+const WildDisplay = () => {
+    return (
+        <div>
+            <h2>Wildlife Photo Graphy</h2>
+        </div>
+    );
+};
+
+export default WildDisplay;
