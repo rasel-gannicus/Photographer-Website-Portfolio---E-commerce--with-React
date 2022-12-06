7939e6b5cbe4ffd3f3a97a8e85923c9111426712
shafik720 <shafikrasel5@gmail.com>
2022-12-06 21:17:13 +0600

    wildlife  popup display done

--- src/Pages/ShowCase/WildDisplay/WildDisplay.js
+++ src/Pages/ShowCase/WildDisplay/WildDisplay.js
@@ -9,28 +9,28 @@ const WildDisplay = () => {
             <hr width='50%' className='text-center mx-auto' />
             <Carousel>             
                 <div className='carousel-img-div'>
-                    <img src="https://i.ibb.co/1XwKWmX/nathan-dumlao-H-c-Zqry-Uuok-unsplash-1.jpg" alt=''/>
+                    <img src="https://i.ibb.co/4gzsMb3/kevin-martin-jose-CRKe-Bz7-M73s-unsplash-1.jpg" alt=''/>
                 </div>                
                 <div className='carousel-img-div'>
-                    <img src="https://i.ibb.co/zfDzPLV/nathan-dumlao-j-HXZZx5-D-lg-unsplash-1.jpg" alt=''/>
+                    <img src="https://i.ibb.co/6FTG7nd/brian-scott-3672q-Py8-V9-Y-unsplash-1.jpg" alt=''/>
                 </div>                
                 <div className='carousel-img-div'>
-                    <img src="https://i.ibb.co/wJLgc2J/nathan-dumlao-5t-WJ49fht-Bw-unsplash-1.jpg" alt=''/>
+                    <img src="https://i.ibb.co/Lhxzc9k/alan-j-hendry-c-TTod-HKs8-NA-unsplash-1.jpg" alt=''/>
                 </div>                
                 <div className='carousel-img-div'>
-                    <img src="https://i.ibb.co/9bnZ4s9/ulyana-tim-Abn-CRg-L2-DNs-unsplash-1.jpg" alt=''/>
+                    <img src="https://i.ibb.co/G0qPDhp/hidde-rensink-Krb-TI4-X1x4-U-unsplash-1.jpg" alt=''/>
                 </div>     
                 <div className='carousel-img-div '>
-                    <img src="https://i.ibb.co/BqRSV64/alok-verma-oh-ENj-R9w0bk-unsplash-1.jpg" alt=''/>
+                    <img src="https://i.ibb.co/YZLDtBK/peter-burdon-k-XEUe-XR4it-Y-unsplash-1.jpg" alt=''/>
                 </div>                
                 <div className='carousel-img-div'>
-                    <img src="https://i.ibb.co/bdnmFHc/sean-williams-d-jy-Me-P6u-NQ-unsplash-2-1.jpg" alt=''/>
+                    <img src="https://i.ibb.co/McHSzrL/georgi-kalaydzhiev-97w-VIopqxow-unsplash-1.jpg" alt=''/>
                 </div>            
                 <div className='carousel-img-div'>
-                    <img src="https://i.ibb.co/k8KTX64/rayyu-maldives-Ku-An-Md-XZav-Y-unsplash-1.jpg" alt=''/>
+                    <img src="https://i.ibb.co/6YzMCxs/marta-rastovac-jn-PZc-Zk9-x-U-unsplash-1.jpg" alt=''/>
                 </div>                    
                 <div className='carousel-img-div'>
-                    <img src="https://i.ibb.co/L1Wqk5H/dollar-gill-T-PUQa-J8-YEw-unsplash-2-1.jpg" alt=''/>
+                    <img src="https://i.ibb.co/WpFTcwG/daniel-lincoln-IE2-Z11z-Ksso-unsplash-1.jpg" alt=''/>
                 </div>              
             </Carousel>
         </div>
