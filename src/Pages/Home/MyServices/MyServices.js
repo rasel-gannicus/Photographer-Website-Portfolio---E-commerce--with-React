96616e62ea77e3afc039a99f0168f5fee1de6ff7
shafik720 <shafikrasel5@gmail.com>
2022-12-07 18:16:15 +0600

    working on my services

--- src/Pages/Home/MyServices/MyServices.js
+++ src/Pages/Home/MyServices/MyServices.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import WeddingService from './WeddingService/WeddingService';
 
 const MyServices = () => {
     return (
         <div className='my-5'>
             <h2>My Services</h2>
+            <WeddingService></WeddingService>
         </div>
     );
 };
---
+++ src/Pages/Home/MyServices/WeddingService/WeddingService.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './WeddingService.css';
+
+const WeddingService = () => {
+    return (
+        <div className='container'>
+            <div className="wedding-card border">
+                <div className="wedding-card-upper border">
+                    <h4>Wedding Upper</h4>
+                </div>
+                <div className="wedding-card-lower border">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque id amet ullam, nemo assumenda laborum incidunt odit atque, libero nobis, nihil omnis magnam. Totam saepe, eius cupiditate reiciendis iure vero.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WeddingService;
