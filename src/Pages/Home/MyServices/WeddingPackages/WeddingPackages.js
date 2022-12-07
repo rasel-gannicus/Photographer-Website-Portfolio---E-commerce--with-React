f1ea0456e9a6103f12fe1e39d7a80ef76c0d613b
shafik720 <shafikrasel5@gmail.com>
2022-12-07 22:25:30 +0600

    working on package data

--- src/Pages/Home/MyServices/WeddingPackages/WeddingPackages.js
+++ src/Pages/Home/MyServices/WeddingPackages/WeddingPackages.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './WeddingPackages.css';
 
 const WeddingPackages = (props) => {
-    const{id, packageCatagory, packageCatagoryName, cameraMan, duration, thumbImg} = props.index;
+    const{id, packageCatagory, packageCatagoryName, cameraMan, duration, thumbImg, price} = props.index;
     return (
         <div className='mx-auto'>
             <div className="wedding-card ">
@@ -11,14 +11,14 @@ const WeddingPackages = (props) => {
                 </div>
                 <div className="wedding-card-lower ">
                     <div className="wedding-card-desc">
-                        <h4>Regular Package</h4>
+                        <h4>{packageCatagoryName}</h4>
                         <div className="package-main-feature">
                             <div className="package-main-feature-left">
-                                <p>01</p>
+                                <p>{cameraMan}</p>
                                 <p>Camera Man</p>
                             </div>
                             <div className="package-main-feature-right">
-                                <p>03</p>
+                                <p>{duration}</p>
                                 <p>Hours Shooting</p>
                             </div>
                         </div>
@@ -37,7 +37,7 @@ const WeddingPackages = (props) => {
                 </div>
                 <div className="wedding-card-icon border">
                     <div className="wedding-card-icon-inner">
-                        <p>$150</p>
+                        <p>${price}</p>
                     </div>
                 </div>
             </div>
