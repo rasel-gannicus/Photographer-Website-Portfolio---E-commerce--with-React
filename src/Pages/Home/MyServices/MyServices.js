1755291751d6a74501ad2d54228c5ff5b3b63ef3
shafik720 <shafikrasel5@gmail.com>
2022-12-07 22:09:30 +0600

    working on package data

--- src/Pages/Home/MyServices/MyServices.js
+++ src/Pages/Home/MyServices/MyServices.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import WeddingPackages from './WeddingPackages/WeddingPackages';
 import WeddingService from './WeddingService/WeddingService';
 
 const MyServices = () => {
+    const[packages, setPackages] = useState([]);
+    useEffect(()=>{
+        fetch('https://raw.githubusercontent.com/shafik720/rest-api/main/photographer-package.json')
+        .then(res=>res.json())
+        .then(data=>setPackages(data))
+    },[])
     return (
         <div className='my-5'>
             <h2>My Services</h2>
-            <WeddingService></WeddingService>
+            {/* <WeddingService></WeddingService> */}
+            <div className="wedding-packages-div">
+                {
+                    packages.map(index=><WeddingPackages
+                        index = {index}
+                        key = {index.id}
+                    ></WeddingPackages>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Pages/Home/MyServices/WeddingPackages/WeddingPackages.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WeddingPackages.css';
+
+const WeddingPackages = (props) => {
+    return (
+        <div>
+            <h2>Wedding package</h2>
+        </div>
+    );
+};
+
+export default WeddingPackages;
