a83675555f1b5abcad19400678ac749ee76b12d0
shafik720 <shafikrasel5@gmail.com>
2022-12-08 11:41:20 +0600

    working on booking storage

---
+++ src/utilities/Local Storage/bookings-storage.js
@@ -0,0 +1,18 @@
+
+
+function addToBookingDb(id){
+    let bookings = JSON.parse(localStorage.getItem('booking') || '{}');
+    if(bookings[id]){
+        bookings[id] = bookings[id]  + 1;
+    }else{
+        bookings[id] = 1 ;
+    }
+    localStorage.setItem('booking', JSON.stringify(bookings));
+}
+
+
+
+
+export{
+    addToBookingDb
+}
