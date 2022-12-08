d7de33e4566f28589d2e67a77beac933523d1371
shafik720 <shafikrasel5@gmail.com>
2022-12-08 12:07:56 +0600

    working on booking storage

--- src/Pages/Home/MyServices/MyServices.js
+++ src/Pages/Home/MyServices/MyServices.js
@@ -6,39 +6,44 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook } from '@fortawesome/free-solid-svg-icons';
 
 const MyServices = () => {
-    const[packages, setPackages] = useState([]);
-    useEffect(()=>{
+    const [packages, setPackages] = useState([]);
+    useEffect(() => {
         fetch('https://raw.githubusercontent.com/shafik720/rest-api/main/photographer-package.json')
-        .then(res=>res.json())
-        .then(data=>setPackages(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setPackages(data))
+    }, [])
 
-    let[booking, setBooking] = useState([]);
-    function handleAddToBooking(id){
+    let [booking, setBooking] = useState([]);
+    function handleAddToBooking(id) {
         addToBookingDb(id);
         let booked = [];
         booked = [...booking, id];
         setBooking(booked);
     }
-    useEffect(()=>{
+    useEffect(() => {
         let bookingCartNumber = document.querySelector('.booking-cart-number');
-        // bookingCartNumber.innerText = booking.length;
-    },[booking])
+        bookingCartNumber.innerText = booking.length;
+    }, [booking])
     return (
         <div className=' myServices-div my-5 mx-auto border'>
             <h2>My Services</h2>
             {/* <WeddingService></WeddingService> */}
             <div className="wedding-packages-div">
                 {
-                    packages.map(index=><WeddingPackages
-                        index = {index}
-                        key = {index.id}
-                        handleAddToBooking = {handleAddToBooking}
+                    packages.map(index => <WeddingPackages
+                        index={index}
+                        key={index.id}
+                        handleAddToBooking={handleAddToBooking}
                     ></WeddingPackages>)
                 }
             </div>
-            <div className="booking-cart-display">
-                <span className='booking-cart-number'><FontAwesomeIcon icon={faBook} /></span>
+            <div className="booking-cart-display-parent">
+                <div className="booking-cart-display">
+                    <div className="booking-cart-number-parent">
+                    <span><FontAwesomeIcon icon={faBook} /></span>
+                        <p  className='booking-cart-number'></p>
+                    </div>
+                </div>
             </div>
         </div>
     );
