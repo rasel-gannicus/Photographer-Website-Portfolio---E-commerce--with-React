afbd44ea676c5a5dbdfd61d41a977e39a909bfe9
shafik720 <shafikrasel5@gmail.com>
2022-12-09 16:27:00 +0600

    working on booking cart show

--- src/Pages/Home/MyServices/BookingCart/BookingCartDisplay/BookingCartDisplay.js
+++ src/Pages/Home/MyServices/BookingCart/BookingCartDisplay/BookingCartDisplay.js
@@ -2,10 +2,22 @@ import React from 'react';
 import './BookingCartDisplay.css';
 
 const BookingCartDisplay = (props) => {
-    
+    const { packageCatagoryName, cameraMan, duration, thumbImg } = props.index;
     return (
         <div>
-            <h2>Booking Cart Display</h2>
+            <div className="cart-card">
+                <div className="cart-card-left">
+                    <div className="cart-left-img">
+                        <img src={thumbImg} alt="" />
+                    </div>
+                    <div className="cart-left-desc">
+                        <p>Plan : {packageCatagoryName} </p>
+                    </div>
+                </div>
+                <div className="cart-card-right">
+                    <h4>Quantity</h4>
+                </div>
+            </div>
         </div>
     );
 };
