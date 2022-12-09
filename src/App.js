3552c17e8d3764c8696ef63d6b3b3b9cdccd3175
shafik720 <shafikrasel5@gmail.com>
2022-12-09 17:53:59 +0600

    working on custom cart

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ import usePackages from './utilities/Hooks/usePackages';
 
 function App() {
 
-  const [packages, setPackages] = usePackages();
-  const [booking, setBooking] = useState([]);
+  const [packages, setPackages] = usePackages(); //--- this custom hook will get the data from API of 'Wedding and Portrait Packages'
+  const [booking, setBooking] = useState([]); //--- it will be updated when user click Add to Booking
 
-  let [cart, setCart] = useState([]);
+  let [cart, setCart] = useState([]); //--- it will get current local storage value
   useEffect(() => {
-    let items = getBookingItems();
+    let items = getBookingItems(); //--- extracting value from local storage
     let freshCart = [];
     for (let id in items) {
       let addedProduct = packages.find(index => index.id == id);
@@ -27,10 +27,11 @@ function App() {
         addedProduct.quantity = items[id];
         freshCart.push(addedProduct);
       }
-      setCart(freshCart);
+      setCart(freshCart); //--- it will get current local storage value
     }
-  }, [packages, booking]);
+  }, [packages, booking]); //--- if user added new Booking(by 'Add to Booking' button), local storage will be updated immediately by this dependency.
 
+  //--- this function have been passed to the 'Add To Booking' button in 'Wedding & Portrait Packages', So that when user add a package to cart, this function will be triggered. 
   function handleAddToBooking(id) {
     addToBookingDb(id);
     let booked = [];
@@ -38,7 +39,7 @@ function App() {
     setBooking(booked);
   }
 
-  // showing or hiding booking cart icon when cart is zero or not.
+  //--- showing or hiding booking cart icon when cart is zero or not. if the Booking Cart icon is zero there will be no Cart Icon in the webpage. 
   useEffect(() => {
     let bookingCartNumber = document.querySelector('.booking-cart-number');
     let bookingCartNumberParent = document.querySelector('.booking-cart-display-parent');
