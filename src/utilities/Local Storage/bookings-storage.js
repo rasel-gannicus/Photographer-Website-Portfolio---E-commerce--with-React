e05a0f87284a30b0f74fe6856a639fd8433cec97
shafik720 <shafikrasel5@gmail.com>
2022-12-09 20:26:08 +0600

    working decrease cart

--- src/utilities/Local Storage/bookings-storage.js
+++ src/utilities/Local Storage/bookings-storage.js
@@ -15,9 +15,18 @@ function getBookingItems(){
     return bookings;
 }
 
+function decreaseFromDb(id){
+    let bookings = JSON.parse(localStorage.getItem('booking') || '{}');
+    if(bookings[id]){
+        bookings[id] = bookings[id]  - 1;
+    }
+    localStorage.setItem('booking', JSON.stringify(bookings));
+}
+
 
 
 export{
     addToBookingDb,
-    getBookingItems
+    getBookingItems, 
+    decreaseFromDb
 }
