ecc4e292480857d8844774baa72237bc540e46a6
shafik720 <shafikrasel5@gmail.com>
2022-12-09 16:14:42 +0600

    working on booking cart show

--- src/Pages/Home/MyServices/BookingCart/BookingCart.js
+++ src/Pages/Home/MyServices/BookingCart/BookingCart.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import usePackages from '../../../../utilities/Hooks/usePackages';
 import './BookingCart.css';
 import {getBookingItems} from '../../../../utilities/Local Storage/bookings-storage'
+import BookingCartDisplay from './BookingCartDisplay/BookingCartDisplay';
 
 const BookingCart = () => {
     const [packages, setPackages] = usePackages();
@@ -23,6 +24,7 @@ const BookingCart = () => {
     return (
         <div>
             <h2>Booking Cart Here : {cart?.length}</h2>
+            <BookingCartDisplay></BookingCartDisplay>
         </div>
     );
 };
---
+++ src/Pages/Home/MyServices/BookingCart/BookingCartDisplay/BookingCartDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './BookingCartDisplay.css';
+
+const BookingCartDisplay = () => {
+    return (
+        <div>
+            <h2>Booking Cart Display</h2>
+        </div>
+    );
+};
+
+export default BookingCartDisplay;
