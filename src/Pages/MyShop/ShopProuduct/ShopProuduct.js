fa1ef18ee1a9257e1cc239a1411fb0184e780a7d
shafik720 <shafikrasel5@gmail.com>
2022-12-11 12:48:15 +0600

    working on product shop

--- src/Pages/MyShop/ShopProuduct/ShopProuduct.js
+++ src/Pages/MyShop/ShopProuduct/ShopProuduct.js
@@ -7,6 +7,9 @@ const ShopProuduct = () => {
             <div className="product-img">
                 <img src="https://i.ibb.co/6PjL5Kb/trevor-buntin-m-Qz-ALy-EB-T4-unsplash-1.jpg" alt="" />
             </div>
+            <div className="product-first-div">
+                <h2>Add To Cart</h2>
+            </div>
         </div>
     );
 };
