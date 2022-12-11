33074d733955ca7dfc830ad95a35b24950c01be9
shafik720 <shafikrasel5@gmail.com>
2022-12-11 12:10:16 +0600

    working on product shop

--- src/Pages/MyShop/MyShop.js
+++ src/Pages/MyShop/MyShop.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './MyShop.css';
+import ShopProuduct from './ShopProuduct/ShopProuduct';
 
 const MyShop = () => {
     return (
         <div className='my-5'>
-            <h2>My Shop Here</h2>
+            <h2 className='my-5'>Some Of My Product <br />You Can Buy To Decorate Your Home</h2>
+            <div className="product-show-div ">
+                <ShopProuduct></ShopProuduct>
+                <ShopProuduct></ShopProuduct>
+                <ShopProuduct></ShopProuduct>
+            </div>
         </div>
     );
 };
