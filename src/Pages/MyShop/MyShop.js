d8586875642a00abf7a4e815189c5eb77f5cf483
shafik720 <shafikrasel5@gmail.com>
2022-12-11 15:17:53 +0600

    working on product

--- src/Pages/MyShop/MyShop.js
+++ src/Pages/MyShop/MyShop.js
@@ -1,19 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './MyShop.css';
 import ShopProuduct from './ShopProuduct/ShopProuduct';
 
 const MyShop = () => {
+    const[product, setProduct] = useState([]);
     useEffect(()=>{
         fetch('https://raw.githubusercontent.com/shafik720/rest-api/main/photographer-product-list.json')
         .then(res=>res.json())
-        .then(data=>console.log(data))
+        .then(data=>setProduct(data))
     },[])
     return (
         <div className='my-5'>
             <h2 className='my-5'>Some Of My Product <br />You Can Buy To Decorate Your Home</h2>
             <div className="product-show-div ">
-                <ShopProuduct></ShopProuduct>
-                <ShopProuduct></ShopProuduct>
                 <ShopProuduct></ShopProuduct>
             </div>
         </div>
