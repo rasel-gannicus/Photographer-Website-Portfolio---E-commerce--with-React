ca44e2a9df41751ec822e7fa7f259d9f8d99f94d
shafik720 <shafikrasel5@gmail.com>
2022-12-11 14:19:54 +0600

    working on product

--- src/Pages/MyShop/ShopProuduct/ShopProuduct.js
+++ src/Pages/MyShop/ShopProuduct/ShopProuduct.js
@@ -5,7 +5,7 @@ const ShopProuduct = () => {
     return (
         <div className='product-img-parent'>
             <div className="product-img">
-                <img src="https://i.ibb.co/6PjL5Kb/trevor-buntin-m-Qz-ALy-EB-T4-unsplash-1.jpg" alt="" />
+                <img src="https://i.ibb.co/wYJfLSQ/amirhossein-soltani-YQWYD7-M2vy-E-unsplash-1.jpg" alt="" />
             </div>
             <div className="product-first-div">
                 <button>Add to Cart</button>
