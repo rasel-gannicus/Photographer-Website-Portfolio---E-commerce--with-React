38f2f8db79eade3b7f489ee725b0f3de025fe5da
shafik720 <shafikrasel5@gmail.com>
2022-12-11 19:53:34 +0600

    working on individual shop

--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import useProduct from '../../utilities/Hooks/useProduct';
+import ShopProuduct from '../MyShop/ShopProuduct/ShopProuduct';
 import './Shop.css';
 
 const Shop = () => {
+    const[product, setProduct] = useProduct();
     return (
         <div>
             <h2>Shop here</h2>
+            <div className="product-show-div ">                
+                {
+                    product.map(index=><ShopProuduct
+                        index = {index}
+                        key = {index.img}
+                    ></ShopProuduct>)
+                }
+            </div>
         </div>
     );
 };
