4ca683fedc26573f24332f47d58dcd0940040958
shafik720 <shafikrasel5@gmail.com>
2022-12-12 13:54:56 +0600

    working on local storage for canvas cart

--- src/Pages/CartIconDisplay/CartIconDisplay.js
+++ src/Pages/CartIconDisplay/CartIconDisplay.js
@@ -4,21 +4,28 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const CartIconDisplay = (props) => {
+    // this function will move user to 'Booking cart' page 
     let navigation = useNavigate();
     function goToBookingPage(){
         let url = '/bookingCart';
         navigation(url);
     }
+    // this will show 'Package Cart' Quantity 
     let quantity = 0;
     props.cart.map(index=>{
         quantity = quantity + index.quantity;
     })
 
+    // this will show 'Product Cart' Quantity 
     let productQuantity = 0;
     props.basket.map(index=>{
         productQuantity = productQuantity + index.quantity;
     })
-    console.log(props.basket);
+    // this function will move user to 'Product cart' page 
+    function goToProductCartPage(){
+        let url = '/productCart';
+        navigation(url);
+    }
     return (
         <div className="booking-cart-display-parent">
                 <div draggable onClick={goToBookingPage} className="booking-cart-display">
@@ -27,7 +34,7 @@ const CartIconDisplay = (props) => {
                         <p  className='booking-cart-number'>{quantity}</p>
                     </div>
                 </div>
-                <div draggable onClick={goToBookingPage} className="booking-cart-display mt-3">
+                <div draggable onClick={goToProductCartPage} className="booking-cart-display mt-3">
                     <div className="booking-cart-number-parent">
                     <span><FontAwesomeIcon icon={faCartShopping} /></span>
                         <p  className='booking-cart-number'>{productQuantity}</p>
