44911f4f17a9035b112a90a2ed0e86f9d257589b
shafik720 <shafikrasel5@gmail.com>
2022-12-12 19:31:12 +0600

    working on catagory

--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -6,18 +6,37 @@ import './Shop.css';
 const Shop = (props) => {
     const[product, setProduct] = useProduct();
 
+    //--- This function will show only 'Urban' catagory
     function showUrban(){
         const allProductDiv = document.querySelector('.all-product');
         allProductDiv.classList.add('hideMe');
 
         const urbanDiv = document.querySelector('.urban-product');
         urbanDiv.classList.remove('hideMe');
+        
+        const streetDiv = document.querySelector('.street-product');
+        streetDiv.classList.add('hideMe');
     }
+    
+    //--- This function will show only 'Street' catagory
+    function showStreet(){
+        const allProductDiv = document.querySelector('.all-product');
+        allProductDiv.classList.add('hideMe');
+
+        const urbanDiv = document.querySelector('.urban-product');
+        urbanDiv.classList.add('hideMe');
+        
+        const streetDiv = document.querySelector('.street-product');
+        streetDiv.classList.remove('hideMe');
+
+    }
+
     return (
         <div>
             <h2>Shop here</h2>
             <div className="catagory my-3">
                 <button onClick={showUrban}>Urban</button>
+                <button onClick={showStreet}>Street Photography</button>
             </div>
             <div className="product-show-div all-product">                
                 {
@@ -39,7 +58,16 @@ const Shop = (props) => {
                 }
             </div>
 
-    
+    {/* -------------- This div will show only 'Street' catagory ------------ */}
+            <div className="product-show-div street-product hideMe">                
+                {
+                    product.map(index=> index.catagory == 'street' && <ShopProuduct
+                        index = {index}
+                        key = {index.img}
+                        addProduct={props.addProduct}
+                    ></ShopProuduct>)
+                }
+            </div>
         </div>
     );
 };
