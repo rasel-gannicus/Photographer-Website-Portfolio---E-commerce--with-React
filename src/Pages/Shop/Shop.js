6122cc4087dd11640d7da84ddf3a2d83e78b1414
shafik720 <shafikrasel5@gmail.com>
2022-12-12 19:39:30 +0600

    working on catagory

--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -1,31 +1,38 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
 import useProduct from '../../utilities/Hooks/useProduct';
 import ShopProuduct from '../MyShop/ShopProuduct/ShopProuduct';
 import './Shop.css';
 
 const Shop = (props) => {
-    const[product, setProduct] = useProduct();
+    const [product, setProduct] = useProduct();
 
     //--- This function will show only 'Urban' catagory
-    function showUrban(){
-        const allProductDiv = document.querySelector('.all-product');
-        allProductDiv.classList.add('hideMe');
+    function showUrban() {
+        const spinner = document.querySelector('.spinners');
+        spinner.classList.remove('hideMe');
 
-        const urbanDiv = document.querySelector('.urban-product');
-        urbanDiv.classList.remove('hideMe');
-        
-        const streetDiv = document.querySelector('.street-product');
-        streetDiv.classList.add('hideMe');
+        setTimeout(() => {
+            spinner.classList.add('hideMe');
+            const allProductDiv = document.querySelector('.all-product');
+            allProductDiv.classList.add('hideMe');
+
+            const urbanDiv = document.querySelector('.urban-product');
+            urbanDiv.classList.remove('hideMe');
+
+            const streetDiv = document.querySelector('.street-product');
+            streetDiv.classList.add('hideMe');
+        }, 500);
     }
-    
+
     //--- This function will show only 'Street' catagory
-    function showStreet(){
+    function showStreet() {
         const allProductDiv = document.querySelector('.all-product');
         allProductDiv.classList.add('hideMe');
 
         const urbanDiv = document.querySelector('.urban-product');
         urbanDiv.classList.add('hideMe');
-        
+
         const streetDiv = document.querySelector('.street-product');
         streetDiv.classList.remove('hideMe');
 
@@ -38,32 +45,33 @@ const Shop = (props) => {
                 <button onClick={showUrban}>Urban</button>
                 <button onClick={showStreet}>Street Photography</button>
             </div>
-            <div className="product-show-div all-product">                
+            <p className="spinners hideMe"><Spinner animation="border" variant="primary" /></p>
+            <div className="product-show-div all-product">
                 {
-                    product.map(index=><ShopProuduct
-                        index = {index}
-                        key = {index.img}
+                    product.map(index => <ShopProuduct
+                        index={index}
+                        key={index.img}
                         addProduct={props.addProduct}
                     ></ShopProuduct>)
                 }
             </div>
-    {/* -------------- This div will show only 'Urban' catagory ------------ */}
-            <div className="product-show-div urban-product hideMe">                
+            {/* -------------- This div will show only 'Urban' catagory ------------ */}
+            <div className="product-show-div urban-product hideMe">
                 {
-                    product.map(index=> index.catagory == 'urban' && <ShopProuduct
-                        index = {index}
-                        key = {index.img}
+                    product.map(index => index.catagory == 'urban' && <ShopProuduct
+                        index={index}
+                        key={index.img}
                         addProduct={props.addProduct}
                     ></ShopProuduct>)
                 }
             </div>
 
-    {/* -------------- This div will show only 'Street' catagory ------------ */}
-            <div className="product-show-div street-product hideMe">                
+            {/* -------------- This div will show only 'Street' catagory ------------ */}
+            <div className="product-show-div street-product hideMe">
                 {
-                    product.map(index=> index.catagory == 'street' && <ShopProuduct
-                        index = {index}
-                        key = {index.img}
+                    product.map(index => index.catagory == 'street' && <ShopProuduct
+                        index={index}
+                        key={index.img}
                         addProduct={props.addProduct}
                     ></ShopProuduct>)
                 }
