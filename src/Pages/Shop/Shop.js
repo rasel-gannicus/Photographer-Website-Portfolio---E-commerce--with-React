15f81cb6b9b2f0a18f2fc5d0198b7978de84f512
shafik720 <shafikrasel5@gmail.com>
2022-12-12 19:53:13 +0600

    working on catagory

--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -44,14 +44,40 @@ const Shop = (props) => {
         }, 500);
     }
 
+    //--- This function will show only 'Street' catagory
+    function showWild() {
+        const spinner = document.querySelector('.spinners');
+        spinner.classList.remove('hideMe');
+
+        setTimeout(() => {
+            spinner.classList.add('hideMe');
+
+            const allProductDiv = document.querySelector('.all-product');
+            allProductDiv.classList.add('hideMe');
+
+            const urbanDiv = document.querySelector('.urban-product');
+            urbanDiv.classList.add('hideMe');
+
+            const streetDiv = document.querySelector('.street-product');
+            streetDiv.classList.add('hideMe');
+            
+            const wildDiv = document.querySelector('.wild-product');
+            wildDiv.classList.remove('hideMe');
+
+        }, 500);
+    }
+
     return (
         <div>
             <h2>Shop here</h2>
             <div className="catagory my-3">
                 <button onClick={showUrban}>Urban</button>
                 <button onClick={showStreet}>Street Photography</button>
+                <button onClick={showWild}>Wild Photography</button>
             </div>
             <p className="spinners hideMe"><Spinner animation="border" variant="primary" /></p>
+
+            {/* -------------- This div will show only 'All' catagory ------------ */}
             <div className="product-show-div all-product">
                 {
                     product.map(index => <ShopProuduct
@@ -82,6 +108,18 @@ const Shop = (props) => {
                     ></ShopProuduct>)
                 }
             </div>
+            
+            {/* -------------- This div will show only 'Wild' catagory ------------ */}
+            <div className="product-show-div wild-product hideMe">
+                {
+                    product.map(index => index.catagory == 'wild' && <ShopProuduct
+                        index={index}
+                        key={index.img}
+                        addProduct={props.addProduct}
+                    ></ShopProuduct>)
+                }
+            </div>
+
         </div>
     );
 };
