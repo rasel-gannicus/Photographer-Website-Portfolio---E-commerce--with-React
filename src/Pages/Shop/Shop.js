88acb2023117c96f5bdbb2f362238895d0a2b117
shafik720 <shafikrasel5@gmail.com>
2022-12-12 19:22:23 +0600

    working on product cart and local storage

--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -5,12 +5,31 @@ import './Shop.css';
 
 const Shop = (props) => {
     const[product, setProduct] = useProduct();
+    function showUrban(element){
+        const allProductDiv = document.querySelector('.all-product');
+        allProductDiv.classList.add('hideMe');
+
+        const urbanDiv = document.querySelector('.urban-product');
+        urbanDiv.classList.remove('hideMe');
+    }
     return (
         <div>
             <h2>Shop here</h2>
-            <div className="product-show-div ">                
+            <div className="catagory my-3">
+                <button onClick={()=>showUrban('urban')}>Urban</button>
+            </div>
+            <div className="product-show-div all-product">                
                 {
                     product.map(index=><ShopProuduct
+                        index = {index}
+                        key = {index.img}
+                        addProduct={props.addProduct}
+                    ></ShopProuduct>)
+                }
+            </div>
+            <div className="product-show-div urban-product hideMe">                
+                {
+                    product.map(index=> index.catagory == 'urban' && <ShopProuduct
                         index = {index}
                         key = {index.img}
                         addProduct={props.addProduct}
