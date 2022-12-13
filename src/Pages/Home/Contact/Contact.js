28bf71bd1ebfdad39b9a6fd583fb6f99d2a0b6ba
shafik720 <shafikrasel5@gmail.com>
2022-12-13 20:30:27 +0600

    working on contact page

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -7,15 +7,23 @@ const Contact = () => {
             <div className="contact-parent container">
                 <div className="row">
                     <div className="col-lg-6">
-                        <div className="">
+                        <div className="h-100">
                             <div className="contact-left">
                                 <img src="https://i.ibb.co/yRFfLG1/craig-manners-bm-Kb-Wks7pps-unsplash.jpg" alt="" />
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-6">
-                        <div className="">
-                            <h2>Contact me</h2>
+                        <div className="h-100">
+                            <div className="contact-form">
+                                <h2>Contact Me </h2>
+                                <div className="input-fields">
+                                    <div className="input-field">
+                                        <input type="text" name="" id="" />
+                                    </div>
+                                    <p>Write Your Name ...</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
