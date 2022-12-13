cbf3713d354746720b0447b77393dcf69242f376
shafik720 <shafikrasel5@gmail.com>
2022-12-13 18:28:44 +0600

    working on cart calculation

--- src/ProductCart/ProductCart.js
+++ src/ProductCart/ProductCart.js
@@ -6,7 +6,8 @@ import ProductCartDisplay from './ProductCartDisplay/ProductCartDisplay';
 const ProductCart = (props) => {
     // console.log(props.basket);
     return (
-        <div className='product-card-parent-div mx-auto my-4'>
+        <div className="">
+            <div className='product-card-parent-div mx-auto my-4'>
             {
                 props.basket.map(index => <ProductCartDisplay
                     index={index}
@@ -17,11 +18,18 @@ const ProductCart = (props) => {
                 ></ProductCartDisplay>)
             }
 
+            {/* showing "Back to Home" when cart is empty */}
             {props.basket.length == 0 && <div className="back-to-homepage">
                 <h2>No item in Cart</h2>
                 <Link to='/'>Back To Home Page</Link>
             </div>}
         </div>
+        
+        <div className="cart-calculation">
+                <h2>Price : </h2>
+            </div>
+        </div>
+        
     );
 };
 
