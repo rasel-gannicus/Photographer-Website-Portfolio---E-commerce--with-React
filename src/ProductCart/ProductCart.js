ca6f9330ad6fc5b191da961b2f21784ecbe5ef0c
shafik720 <shafikrasel5@gmail.com>
2022-12-13 18:25:08 +0600

    working on cart and local storage

--- src/ProductCart/ProductCart.js
+++ src/ProductCart/ProductCart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './ProductCart.css';
 import ProductCartDisplay from './ProductCartDisplay/ProductCartDisplay';
 
@@ -7,14 +8,19 @@ const ProductCart = (props) => {
     return (
         <div className='product-card-parent-div mx-auto my-4'>
             {
-                props.basket.map(index=><ProductCartDisplay
-                    index = {index}
-                    key = {index.img}
+                props.basket.map(index => <ProductCartDisplay
+                    index={index}
+                    key={index.img}
                     addProduct={props.addProduct}
                     decreaseProduct={props.decreaseProduct}
                     deleteProduct={props.deleteProduct}
                 ></ProductCartDisplay>)
             }
+
+            {props.basket.length == 0 && <div className="back-to-homepage">
+                <h2>No item in Cart</h2>
+                <Link to='/'>Back To Home Page</Link>
+            </div>}
         </div>
     );
 };
