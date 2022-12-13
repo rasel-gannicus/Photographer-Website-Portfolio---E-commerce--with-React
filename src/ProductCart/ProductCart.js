a57735b31cc9a98b1a27a677190b806509172c0e
shafik720 <shafikrasel5@gmail.com>
2022-12-13 19:17:12 +0600

    working on cart calculation

--- src/ProductCart/ProductCart.js
+++ src/ProductCart/ProductCart.js
@@ -4,61 +4,64 @@ import './ProductCart.css';
 import ProductCartDisplay from './ProductCartDisplay/ProductCartDisplay';
 
 const ProductCart = (props) => {
-    
-    let price = 0;    
+
+    let price = 0;
     let quantity = 0;
     let shipping = 0;
-    props.basket.map(index=>{
+    let totalCost = 0;
+    props.basket.map(index => {
         price = price + (index.price * index.quantity);
-        quantity = quantity + index.quantity;        
+        quantity = quantity + index.quantity;
     })
-    if(quantity<=5){
+    if (quantity <= 5) {
         shipping = 50 * quantity;
-    }else{
+    } else {
         shipping = 50 * 5;
     }
-    
+    totalCost = shipping + price;
+
     return (
         <div className="">
             <div className='product-card-parent-div mx-auto my-4'>
-            {
-                props.basket.map(index => <ProductCartDisplay
-                    index={index}
-                    key={index.img}
-                    addProduct={props.addProduct}
-                    decreaseProduct={props.decreaseProduct}
-                    deleteProduct={props.deleteProduct}
-                ></ProductCartDisplay>)
-            }
+                {
+                    props.basket.map(index => <ProductCartDisplay
+                        index={index}
+                        key={index.img}
+                        addProduct={props.addProduct}
+                        decreaseProduct={props.decreaseProduct}
+                        deleteProduct={props.deleteProduct}
+                    ></ProductCartDisplay>)
+                }
 
-            {/* showing "Back to Home" when cart is empty */}
-            {props.basket.length == 0 && <div className="back-to-homepage">
-                <h2>No item in Cart</h2>
-                <Link to='/'>Back To Home Page</Link>
+                {/* showing "Back to Home" when cart is empty */}
+                {props.basket.length == 0 && <div className="back-to-homepage">
+                    <h2>No item in Cart</h2>
+                    <Link to='/'>Back To Home Page</Link>
+                </div>}
+            </div>
+
+            {props.basket.length != 0 && <div className="cart-calculation my-5">
+                <table className='mx-auto'>
+                    <tr>
+                        <td>Total Quantity :</td>
+                        <td>{quantity}</td>
+                    </tr>
+                    <tr>
+                        <td>Total Price : </td>
+                        <td>{price}</td>
+                    </tr>
+                    <tr className='last-tr'>
+                        <td>Shipping Charge : <br /> <small className='text-primary'>(For Order more than 5 unit the Shipping charge will be Free)</small> </td>
+                        <td>{shipping}</td>
+                    </tr>
+                    <tr className='fw-bold fs-5'>
+                        <td>Total Cost : </td>
+                        <td> {totalCost} </td>
+                    </tr>
+                </table>
             </div>}
         </div>
-        
-        <div className="cart-calculation my-5">
-            <table className='mx-auto'>
-                <tr>
-                    <td>Price</td>
-                    <td>{price}</td>
-                </tr>
-                <tr>
-                    <td>Total Quantity :</td>
-                    <td>{quantity}</td>
-                </tr>
-                <tr>
-                    <td>Shipping Charge : <br /> <small className='text-primary'>(For Order more than 5 unit the Shipping charge will be Free)</small> </td>
-                    <td>{shipping}</td>
-                </tr>
-            </table>
-                {/* <p>Price : {price} </p>
-                <p> Total Quantity : {quantity} </p>
-                <p>Shipping Charge : </p> */}
-            </div>
-        </div>
-        
+
     );
 };
 
