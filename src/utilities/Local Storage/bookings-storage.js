56f0786fa4ae3c0d7f13d1b39fd1157e7ee66ea4
shafik720 <shafikrasel5@gmail.com>
2022-12-13 14:29:47 +0600

    working on cart and local storage

--- src/utilities/Local Storage/bookings-storage.js
+++ src/utilities/Local Storage/bookings-storage.js
@@ -1,42 +1,51 @@
 
 
-function addToBookingDb(id){
+function addToBookingDb(id) {
     let bookings = JSON.parse(localStorage.getItem('booking') || '{}');
-    if(bookings[id]){
-        if(bookings[id]<4){
-            bookings[id] = bookings[id]  + 1;
-        }        
-    }else{
-        bookings[id] = 1 ;
+    if (bookings[id]) {
+        if (bookings[id] < 4) {
+            bookings[id] = bookings[id] + 1;
+        }
+    } else {
+        bookings[id] = 1;
     }
     localStorage.setItem('booking', JSON.stringify(bookings));
 }
 
-function getBookingItems(){
+function getBookingItems() {
     let bookings = JSON.parse(localStorage.getItem('booking') || '{}');
     return bookings;
 }
 
-function decreaseFromDb(id){
+function decreaseFromDb(id) {
     let bookings = JSON.parse(localStorage.getItem('booking') || '{}');
-    if(bookings[id]){
-        bookings[id] = bookings[id]  - 1;
+    if (bookings[id]) {
+        bookings[id] = bookings[id] - 1;
+    }
+    let size = Object.keys(bookings).length;
+    if (size == 1) {
+        if (bookings[id] <= 0) {
+            delete bookings[id];
+            window.location.reload();
+        }
     }
     if(bookings[id]<=0){
         delete bookings[id];
     }
+    
+    // console.log(Object.keys(bookings).length)
     localStorage.setItem('booking', JSON.stringify(bookings));
 }
 
-function deleteFromDb(){
+function deleteFromDb() {
     localStorage.removeItem('booking');
 }
 
 
 
-export{
+export {
     addToBookingDb,
-    getBookingItems, 
+    getBookingItems,
     deleteFromDb,
     decreaseFromDb
 }
