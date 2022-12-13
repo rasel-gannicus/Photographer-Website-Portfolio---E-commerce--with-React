4ff703e5ffaa0dcb340000ff3b1e39a7722ada4e
shafik720 <shafikrasel5@gmail.com>
2022-12-13 12:24:34 +0600

    working on cart and local storage

--- src/ProductCart/ProductCartDisplay/ProductCartDisplay.js
+++ src/ProductCart/ProductCartDisplay/ProductCartDisplay.js
@@ -1,14 +1,25 @@
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './ProductCartDisplay.css';
 
 const ProductCartDisplay = (props) => {
     const { price, img, quantity } = props.index;
-    console.log(props.index);
+    let costPerUnit = quantity * price;
+
+    
     return (
         <div>
             <div className="product-card">
                 <img src={img} alt="" />
-                <p>Price Per Unit : <span className='bold-text'>$ {props.index.price}</span> </p>
+                <p className='mt-3'>Price Per Unit : <span className='bold-text'>$ {props.index.price}</span> </p>
+                <p>Quantity : {quantity} </p>
+                <p>Cost : {costPerUnit} </p>
+                <div className="increment-div my-3 mx-auto">
+                        <span draggable className='increment-icon'><FontAwesomeIcon icon={faMinus} /></span>
+                        <div> <span className='blue-text'>{quantity}</span>  Unit </div>
+                        <span draggable className='increment-icon'><FontAwesomeIcon icon={faPlus} /></span>
+                    </div>
             </div>
         </div>
     );
