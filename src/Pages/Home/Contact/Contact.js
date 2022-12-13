9d7001205ca15fdae54f4f3b369077b3318b36fe
shafik720 <shafikrasel5@gmail.com>
2022-12-13 21:58:40 +0600

    contact page done

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -4,11 +4,11 @@ import './Contact.css';
 const Contact = () => {
     return (
         <div className='my-5'>
-            <div className="contact-parent container">
-                <div className="row">
+            <div className="contact-parent container py-5">
+                <div className="row g-4">
                     <div className="col-lg-6">
                         <div className="h-100">
-                            <div className="contact-left">
+                            <div className="contact-left h-100">
                                 <img src="https://i.ibb.co/yRFfLG1/craig-manners-bm-Kb-Wks7pps-unsplash.jpg" alt="" />
                             </div>
                         </div>
@@ -16,7 +16,7 @@ const Contact = () => {
                     <div className="col-lg-6">
                         <div className="h-100">
                             <div className="contact-form">
-                                <h2 className='my-3 fw-bold'>Contact Me </h2>
+                                <h2 className='mb-3 fw-bold'>Contact Me </h2>
                                 <div className="input-fields">
                                     <form action="">
                                         <div className="input-field ">
