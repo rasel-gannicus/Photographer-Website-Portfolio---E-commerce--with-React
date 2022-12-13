c1a1d5f30e3acadd9ebc3f4e2751ffbb86c09af8
shafik720 <shafikrasel5@gmail.com>
2022-12-13 20:27:49 +0600

    working on contact page

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -4,7 +4,22 @@ import './Contact.css';
 const Contact = () => {
     return (
         <div className='my-5'>
-            <h2>Contact here</h2>
+            <div className="contact-parent container">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <div className="">
+                            <div className="contact-left">
+                                <img src="https://i.ibb.co/yRFfLG1/craig-manners-bm-Kb-Wks7pps-unsplash.jpg" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="">
+                            <h2>Contact me</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
