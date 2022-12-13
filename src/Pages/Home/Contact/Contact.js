7af2139c7807682c6c2436ff8c54a6956072f1ff
shafik720 <shafikrasel5@gmail.com>
2022-12-13 20:18:39 +0600

    working on contact page

---
+++ src/Pages/Home/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
+    return (
+        <div className='my-5'>
+            <h2>Contact here</h2>
+        </div>
+    );
+};
+
+export default Contact;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,6 +3,7 @@ import About from '../About/About';
 import MyShop from '../MyShop/MyShop';
 import ShowCase from '../ShowCase/ShowCase';
 import Banner from './Banner/Banner';
+import Contact from './Contact/Contact';
 import './Home.css';
 import MyServices from './MyServices/MyServices';
 
@@ -14,6 +15,7 @@ const Home = (props) => {
             <ShowCase></ShowCase>
             <MyServices handleAddToBooking={props.handleAddToBooking}></MyServices>
             <MyShop addProduct={props.addProduct}></MyShop>
+            <Contact></Contact>
         </div>
     );
 };
