4def25c8b503953c7867ca3fbc84e1de38636fbc
shafik720 <shafikrasel5@gmail.com>
2022-12-14 20:07:57 +0600

    working on footer

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -3,6 +3,14 @@ import React from 'react';
 import './Footer.css';
 import facebookLogo from '../../Logo Icon/facebook.svg'
 import facebookLogo2 from '../../Logo Icon/facebook (1).svg'
+import twitter from '../../Logo Icon/twitter (1).svg'
+import twitter2 from '../../Logo Icon/twitter (3).svg'
+import instagram from '../../Logo Icon/instagram.svg'
+import instagram2 from '../../Logo Icon/instagram (1).svg'
+import whatsapp from '../../Logo Icon/whatsapp.svg'
+import whatsapp2 from '../../Logo Icon/whatsapp (1).svg'
+import linkedIn from '../../Logo Icon/likedin.svg'
+import linkedIn2 from '../../Logo Icon/likedin (1).svg'
 
 const Footer = () => {
     return (
@@ -12,6 +20,22 @@ const Footer = () => {
                 <div className="social-div fb-icon">
                     <span className='first-logo'><img src={facebookLogo} alt="" /></span>
                     <span className='second-logo'><img src={facebookLogo2} alt="" /></span>
+                </div>
+                <div className="social-div fb-icon">
+                    <span className='first-logo'><img src={twitter} alt="" /></span>
+                    <span className='second-logo'><img src={twitter2} alt="" /></span>
+                </div>
+                <div className="social-div fb-icon">
+                    <span className='first-logo'><img src={instagram} alt="" /></span>
+                    <span className='second-logo'><img src={instagram2} alt="" /></span>
+                </div>
+                <div className="social-div fb-icon">
+                    <span className='first-logo'><img src={whatsapp} alt="" /></span>
+                    <span className='second-logo'><img src={whatsapp2} alt="" /></span>
+                </div>
+                <div className="social-div fb-icon">
+                    <span className='first-logo'><img src={linkedIn} alt="" /></span>
+                    <span className='second-logo'><img src={linkedIn2} alt="" /></span>
                 </div>
             </div>
         </div>
