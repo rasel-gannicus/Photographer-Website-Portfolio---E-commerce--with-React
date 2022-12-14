d9cddd1a8aee6050755e3e2357648fe28a8900d3
shafik720 <shafikrasel5@gmail.com>
2022-12-14 19:42:23 +0600

    working on footer

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,10 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Footer.css';
+import facebookLogo from '../../Logo Icon/facebook.svg'
 
 const Footer = () => {
     return (
-        <div>
-            <h2>Footer Here</h2>
+        <div className='footer mt-5'>
+            <div className="footer-upper"></div>
+            <div className="footer-bottom">
+                <div className="social-div fb-icon">
+                    <span><img src={facebookLogo} alt="" /></span>
+                </div>
+            </div>
         </div>
     );
 };
