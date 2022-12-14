f8d332d0d26f3a1f51f8c77776ebb256aee6d6ec
shafik720 <shafikrasel5@gmail.com>
2022-12-14 20:09:57 +0600

    working on footer

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -15,8 +15,7 @@ import linkedIn2 from '../../Logo Icon/likedin (1).svg'
 const Footer = () => {
     return (
         <div className='footer mt-5'>
-            <div className="footer-upper"></div>
-            <div className="footer-bottom">
+            <div className="footer-upper">
                 <div className="social-div fb-icon">
                     <span className='first-logo'><img src={facebookLogo} alt="" /></span>
                     <span className='second-logo'><img src={facebookLogo2} alt="" /></span>
