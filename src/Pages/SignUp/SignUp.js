1cebe8a97242af5463069290fc27628d36e0756b
shafik720 <shafikrasel5@gmail.com>
2022-12-16 21:30:49 +0600

    sdfsfg

---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div>
+            <h2>Sign Up here</h2>
+        </div>
+    );
+};
+
+export default SignUp;<h2>Sign Up here</h2>
