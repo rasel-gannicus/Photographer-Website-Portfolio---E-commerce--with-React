7caa23b162cfd1044da5c9129027c3fbd93e5f7a
shafik720 <shafikrasel5@gmail.com>
2022-12-17 12:33:02 +0600

    working on login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -8,47 +8,43 @@ import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const navigate = useNavigate();
-    function navigation(){
+    function navigation() {
         navigate('/signup');
     }
     return (
-        <div className='container'>
-            <div className="row">
-                <div className="col-lg-4 mx-auto bg-white py-5 px-4 login-div">
-                    <div className="">
-                        <div className="input-fields">
-                            <form action="">                                
-                                <div className="input-field ">
-                                    <input type="text" name="" id="" required />
-                                    <span className='input-placeholder'>Your Email </span>
-                                </div>
-                                <div className="input-field ">
-                                    <input type="password" name="" id="" required />
-                                    <span className='input-placeholder'>Your password </span>
-                                </div>
-                                <div className="message-button-div login-button-div">
-                                    <button>Login</button>
-                                </div>
-                            </form>
-                            <p>Don't have an account ? <span onClick={navigation} draggable className='text-danger red-text'>Register Here</span> </p>
-                            <div className='or-div'>
-                                <hr />
-                                <p>Or</p>
-                            </div>
-                            <div className="social-login-div">
-                                <p>Sign in using</p>
-                                <div className="social-login-div-icon">
-                                    <div className="social-login">
-                                        <img src={googleLogo} alt="" />
-                                    </div>
-                                    <div className="social-login">
-                                        <img src={facebookLogo} alt="" />
-                                    </div>
-                                    <div className="social-login">
-                                        <img src={githubLogo} alt="" />
-                                    </div>
-                                </div>
-                            </div>
+        <div>
+            <div className="input-fields mx-auto login-div ">
+                <h4>Login</h4>
+                <hr width='70%' className='mx-auto' />
+                <form action="">
+                    <div className="input-field ">
+                        <input type="text" name="" id="" required />
+                        <span className='input-placeholder'>Your Email </span>
+                    </div>
+                    <div className="input-field ">
+                        <input type="password" name="" id="" required />
+                        <span className='input-placeholder'>Your password </span>
+                    </div>
+                    <div className="message-button-div login-button-div">
+                        <button>Login</button>
+                    </div>
+                </form>
+                <p>Don't have an account ? <span onClick={navigation} draggable className='text-danger red-text'>Register Here</span> </p>
+                <div className='or-div'>
+                    <hr />
+                    <p>Or</p>
+                </div>
+                <div className="social-login-div">
+                    <p>Sign in using</p>
+                    <div className="social-login-div-icon">
+                        <div className="social-login">
+                            <img src={googleLogo} alt="" />
+                        </div>
+                        <div className="social-login">
+                            <img src={facebookLogo} alt="" />
+                        </div>
+                        <div className="social-login">
+                            <img src={githubLogo} alt="" />
                         </div>
                     </div>
                 </div>
