07413a6ac6c016ee07b66af6faf87584bc0bfb9d
shafik720 <shafikrasel5@gmail.com>
2022-12-17 11:32:27 +0600

    working on login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -5,7 +5,7 @@ const Login = () => {
     return (
         <div className='container'>
             <div className="row">
-                <div className="col-lg-6 mx-auto bg-white py-5 px-4 login-div">
+                <div className="col-lg-4 mx-auto bg-white py-5 px-4 login-div">
                     <div className="">
                         <div className="input-fields">
                             <form action="">                                
@@ -17,10 +17,11 @@ const Login = () => {
                                     <input type="password" name="" id="" required />
                                     <span className='input-placeholder'>Your password </span>
                                 </div>
-                                <div className="message-button-div ">
+                                <div className="message-button-div login-button-div">
                                     <button>Login</button>
                                 </div>
                             </form>
+                            <p>Don't have an account ? <span className='text-danger red-text'>Register Here</span> </p>
                         </div>
                     </div>
                 </div>
