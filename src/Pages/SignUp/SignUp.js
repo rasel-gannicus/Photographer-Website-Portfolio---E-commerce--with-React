be3a9be3d198538235abe7ef82374f43fcdab93f
shafik720 <shafikrasel5@gmail.com>
2022-12-17 18:29:26 +0600

    working on firebase auth

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './SignUp.css';
 import googleLogo from '../../Logo Icon/google.svg';
 import facebookLogo from '../../Logo Icon/facebook (1).svg';
 import githubLogo from '../../Logo Icon/github.svg';
@@ -25,8 +26,15 @@ const SignUp = () => {
         setRepassword(e.target.value);
     }
 
+    let errorText = document.querySelector('.error-message p');
     function handleSubmit(e){
         e.preventDefault();
+        if(password.length<6){
+            errorText.innerText = 'Password length must be more than 6 character'
+        }else if(password !== repassword){
+            errorText.innerText = 'Password is not matched !'
+        }
+        
     }
 
     return (
@@ -50,6 +58,9 @@ const SignUp = () => {
                     <div className="input-field ">
                         <input onBlur={handleRepassword} type="password" name="" id="" required />
                         <span className='input-placeholder'>Confirm password </span>
+                    </div>
+                    <div className="error-message">
+                        <p></p>
                     </div>
                     <div className="message-button-div login-button-div">
                         <button>Login</button>
