e876dd1df4d3ca4e0ba4b5deeb73d86b7743cdd8
shafik720 <shafikrasel5@gmail.com>
2022-12-18 11:13:55 +0600

    working on login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
 import googleLogo from '../../Logo Icon/google.svg';
@@ -7,19 +7,32 @@ import githubLogo from '../../Logo Icon/github.svg';
 import { useNavigate } from 'react-router-dom';
 import { useSignInWithFacebook, useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../utilities/firebase.init';
+import { Spinner } from 'react-bootstrap';
 
 const Login = () => {
     const navigate = useNavigate();
     function navigation() {
         navigate('/signup');
     }
-
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+     // this function will get email from user and will reserve it to 'email' state
+     function handleEmail(e){
+        setEmail(e.target.value);
+    }
+    // this function will get password from user and will reserve it to 'password' state
+    function handlePassword(e){
+        setPassword(e.target.value);
+    }
     // sign up with 'google' 
     const [signInWithGoogle, user2, loading2, error2] = useSignInWithGoogle(auth);
     // sign up with 'facebook' 
     const [signInWithFacebook, user3, loading3, error3] = useSignInWithFacebook(auth);
     // sign up with 'github' 
     const [signInWithGithub, user4, loading4, error4] = useSignInWithGithub(auth);
+
+    let errorText = document.querySelector('.error-message p');
+    let spinnerSignup = document.querySelector('.spinner-signup');
     return (
         <div>
             <div className="input-fields mx-auto login-div ">
@@ -33,6 +46,11 @@ const Login = () => {
                     <div className="input-field ">
                         <input type="password" name="" id="" required />
                         <span className='input-placeholder'>Your password </span>
+                    </div>
+                    {/* ------------------ Error message will be shown here ----------------- */}
+                    <div className="error-message">
+                        <span className='spinner-signup'><Spinner animation="border"  variant="primary" /></span>
+                        <p></p>
                     </div>
                     <div className="message-button-div login-button-div">
                         <button>Login</button>
