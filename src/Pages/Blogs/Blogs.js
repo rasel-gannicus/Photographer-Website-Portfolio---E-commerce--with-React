c4bb9a6c38e26974fe7b028105ad6611745ecb2f
shafik720 <shafikrasel5@gmail.com>
2022-12-21 22:41:40 +0600

    working on blog

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -10,7 +10,7 @@ const Blogs = () => {
                     <div className="">
                         <div className='blog-div'>
                             <div className="blog-heading">
-                                <h2>Blog Heading</h2>
+                                <p>What's the difference between authorization and authentication ?</p>
                             </div>
                             <div className="blog-body">
                                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde incidunt facere tenetur temporibus perferendis possimus cupiditate officiis similique, eum itaque dolores odit nulla. Quam consequatur, exercitationem est deserunt mollitia non amet odio ipsa sit tempore nam necessitatibus dolor possimus libero aliquid temporibus provident obcaecati odit, quasi rem quaerat. Voluptas, quod!</p>
