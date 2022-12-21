2488289aec092f054b294f8013afc19d1580488b
shafik720 <shafikrasel5@gmail.com>
2022-12-21 22:21:22 +0600

    working on blog

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Blogs.css';
+import profileLogo from '../../utilities/img/bg-low-size.png';
 
 const Blogs = () => {
     return (
@@ -15,6 +16,9 @@ const Blogs = () => {
                                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde incidunt facere tenetur temporibus perferendis possimus cupiditate officiis similique, eum itaque dolores odit nulla. Quam consequatur, exercitationem est deserunt mollitia non amet odio ipsa sit tempore nam necessitatibus dolor possimus libero aliquid temporibus provident obcaecati odit, quasi rem quaerat. Voluptas, quod!</p>
                             </div>
                             <div className="blog-footer">
+                                <div className="blog-footer-img">
+                                    <img src={profileLogo} alt=""/>
+                                </div>
                                 <h2>Blog Footer</h2>
                             </div>
                         </div>
