d7f1f1c69ca4ce3bd4d6a46816b0d9a00bcf1910
shafik720 <shafikrasel5@gmail.com>
2022-12-22 21:10:01 +0600

    working on reset password

--- src/Pages/ResetPassword/ResetPassword.js
+++ src/Pages/ResetPassword/ResetPassword.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 
 const ResetPassword = () => {
+    const[email, setEmail] = useState('');
+    // this function will get email from user and will reserve it to 'email' state
+    function handleEmail(e){
+        setEmail(e.target.value);
+    }
     return (
         <div>
-            <h2>reset Password here</h2>
+            <div className="input-fields mx-auto login-div ">
+                <h4>Login</h4>
+                <hr width='70%' className='mx-auto' />
+                <form action="" onSubmit={handleSubmit}>
+                    <div className="input-field ">
+                        <input onBlur={handleEmail} type="text" name="" id="" required />
+                        <span className='input-placeholder'>Your Email </span>
+                    </div>
+                    {/* ------------------ Error message will be shown here ----------------- */}
+                    <div className="error-message">
+                        <span className='spinner-signup'><Spinner animation="border"  variant="primary" /></span>
+                        <p></p>
+                    </div>
+                    <div className="message-button-div login-button-div">
+                        <button>Login</button>
+                    </div>
+                </form>
+                
+            </div>
         </div>
     );
 };
